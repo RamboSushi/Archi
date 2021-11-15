c036ac1f06e48893ae9862e32511137c95236a3b
elie.kazan <elie.kazan@ens.uvsq.fr>
2021-11-15 10:27:42 +0100

    ajout de javadoc

--- src/modele/Classe.java
+++ src/modele/Classe.java
@@ -4,6 +4,14 @@ public class Classe {
 	private String nomination;
 	private AnneeScolaire anneeScolaire;
 	
+	/**
+	 * Constructeur de la classe Classe qui met la 
+	 * valeur du parametre nomination dans l'attribut nomination
+	 * et met la valeur d'anneeDepart dans l'attribut 
+	 * correpondant
+	 * @param nomination
+	 * @param anneeDepart
+	 */
 	public Classe(String nomination, int anneeDepart) {
 		this.nomination = nomination;
 		this.anneeScolaire = new AnneeScolaire(anneeDepart);
@@ -13,25 +21,51 @@ public class Classe {
 		this.anneeScolaire = new AnneeScolaire(anneeDepart);
 	}
 	
+	/**
+	 * retourne l'attribut nomination
+	 * @return une chaine de caractere representant la nomination
+	 */
 	public String getNomination() {
 		return nomination;
 	}
+	
+	/**
+	 * Met la valeur de nomination dans l'attribut nomination
+	 * @param nomination
+	 */
 	public void setNomination(String nomination) {
 		this.nomination = nomination;
 	}
 	
+	/**
+	 * Retourne l'attribut anneeScolaire
+	 * @return une AnnneScolaire representant l'attribut anneeScolaire
+	 */
 	public AnneeScolaire getAnneeScolaire() {
 		return anneeScolaire;
 	}
+	
+	/**
+	 * Met la valeur d'anneScolaire dans l'attribut anneeScolaire
+	 * @param anneeScolaire
+	 */
 	public void setAnneeScolaire(AnneeScolaire anneeScolaire) {
 		this.anneeScolaire = anneeScolaire;
 	}
 	
 	@Override
+	/**
+	 * Affiche les attribut de la forme nomination anneeScolaire
+	 */
 	public String toString() {
 		return nomination + ":" + anneeScolaire;
 	}
 	
+	/**
+	 * 
+	 * @param classe
+	 * @return un booleen 
+	 */
 	public boolean equals(Classe classe) {
 		if (nomination.equals(classe.getNomination()) &&
 			anneeScolaire.equals(anneeScolaire)) {
@@ -40,6 +74,11 @@ public class Classe {
 		return false;
 	}
 	
+	/**
+	 * retourne l'attribut nomination avec l'appel de la 
+	 * methode parse sur l'attribut anneeScolaire
+	 * @return String
+	 */
 	public String parse() {
 		return nomination + ";" + anneeScolaire.parse();
 	}
